42d45529dcf444501d0c0dafca89d177f684f4a4
Parish Neighbors <pn8bors@gmail.com>
2022-02-09 11:36:52 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,29 @@
+import got from 'got';
+
+/**
+ * Get a user's repos off of GitHub
+ * @param {string} username
+ * @param {function} callback This will return an error and an array of repositories
+ * Documentation: https://docs.github.com/en/rest/reference/repos#list-repositories-for-a-user
+ */
+async function github(username, callback) {
+    let err, data;
+    try {
+        const resp = got(`https://api.github.com/users/${username}/repos`)
+        data = await resp.json();
+        callback(err, data);
+    } catch (err) {
+        callback(err, data)
+    }
+}
+
+function main() {
+    github('PaN8bors', (error, projects) => {
+        for (let project of projects) {
+            console.log(`${project.name} has ${project.stargazers_count} stars.`)
+        }
+        console.log(data);
+    });
+}
+
+main();
